1c123603ccfdb8855d8b069c7e44553daf14e8f0
hebinjay <65268119+hebinjay@users.noreply.github.com>
2021-08-19 06:27:57 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# MNK_vue
-成都莫尼卡
+# atmos_ins_plat
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your unit tests
+```
+npm run test:unit
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+ publicPath: process.env.NODE_ENV === 'production'
+     ? './'
+     : '/',
+ devServer: {
+  port: 8080, // 端口号配置  //解决axios跨域问题	
+ // autoOpenBrowser: true,
+ // assetsSubDirectory: 'static',
+ // assetsPublicPath: '/',
+  proxy: {
+              '': {
+                  target: 'http://localhost:8080',
+                  changeOrigin: true,
+                  ws: true,
+                  pathRewrite: {						
+                  }
+              }
+          } 
+ },
+ configureWebpack: {
+  externals: {
+	'AMap': 'AMap', // 高德地图配置
+	'Loca': 'Loca',
+	'AMapUI': 'AMapUI',
+	'$':'jquery',
+	'jQuery':'jquery'
+  }
+ },
+}
